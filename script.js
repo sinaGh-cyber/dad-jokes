579d6ed89bea150551a74586b569f1d865a60faa
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-10-06 02:17:12 +0330

    Js ready - final

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
+const getJoke = async () => {
+  const config = {
+    headers: {
+      accept: 'application/json',
+    },
+  };
+
+  const response = await fetch('https://icanhazdadjoke.com', config);
+  const data = await response.json();
+  jokeEl.textContent = data.joke;
+};
+
+jokeBtn.addEventListener('click', getJoke);
+
+getJoke();
